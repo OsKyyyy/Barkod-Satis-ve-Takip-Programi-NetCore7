1828c9314402163e82b50df335c0037adbf8c5a6
oğuzhan inan <oguzhaninan@outlook.com>
2024-06-21 15:08:07 +0300

    21.06 15:07

---
+++ WebUI/wwwroot/assets/js/pages/report/incomeAndExpensesRequest.js
@@ -0,0 +1,27 @@
+﻿var IncomeAndExpensesRequest = {
+
+    Init: function () {
+        IncomeAndExpensesRequest.EstimatedMonthlyIncome();              
+    },
+
+    EstimatedMonthlyIncome: function () {       
+
+        $.ajax({
+            type: "POST",
+            url: "IncomeAndExpensesReport?handler=IncomeAndExpensesTable",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            data: JSON.stringify(null),
+            headers: {
+                RequestVerificationToken:
+                    $('input:hidden[name="__RequestVerificationToken"]').val()
+            },
+            success: function (response) {
+                                               
+            },
+        })
+    },      
+}
+$(document).ready(function () {
+    IncomeAndExpensesRequest.Init();
+});
